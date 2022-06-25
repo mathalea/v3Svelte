601ba080085577ed62dcdb744ac60523ec44dfce
sylcha <chambon.sylvain@gmail.com>
2022-06-25 20:23:50 +0200

    feat: boutons et style avec tailwind

--- src/components/BoutonDescendre.svelte
+++ src/components/BoutonDescendre.svelte
@@ -1,17 +1,23 @@
+<button on:click={descendre}>
+  <i class="bx bx-down-arrow-circle ml-1" />
+</button>
+
 <script lang="ts">
-    import { listeExercices } from "./store";
+  import { listeExercices } from "./store";
 
-    export let indice: number;
-    function descendre() {
-        listeExercices.update((l) => {
-            return moveExercice(l, indice, indice + 1)
-        });
-    }
+  export let indice: number;
+  function descendre() {
+    listeExercices.update((l) => {
+      return moveExercice(l, indice, indice + 1);
+    });
+  }
 
-    function moveExercice(liste: any[], iDepart: number, iArrivee: number): any[] {
-        liste.splice(iArrivee, 0, liste.splice(iDepart, 1)[0])
-        return liste;
-    }
+  function moveExercice(
+    liste: any[],
+    iDepart: number,
+    iArrivee: number
+  ): any[] {
+    liste.splice(iArrivee, 0, liste.splice(iDepart, 1)[0]);
+    return liste;
+  }
 </script>
-
-<button on:click={descendre}> ⬇︎ </button>
--- src/components/Exercice.svelte
+++ src/components/Exercice.svelte
@@ -1,87 +1,118 @@
+<div>
+  <!-- <button
+    type="button"
+    on:click={() => {
+      visible = !visible;
+    }}>{visible ? "Cacher" : "Montrer"}</button
+  > -->
+</div>
+<div bind:this={divExercice}>
+  <h1
+    class="border-b border-gray-300 text-orange-600 font-semibold text-left pl-4 text-xl mt-4 pb-2 flex flex-row items-center"
+  >
+    Exercice {indiceExercice + 1}
+    <button
+      type="button"
+      on:click={() => {
+        visible = !visible;
+      }}
+    >
+      {#if visible}
+        <i class="bx bx-hide ml-6" />
+      {:else}
+        <i class="bx bx-show ml-6" />
+      {/if}
+    </button>
+    <button type="button" on:click={newData}
+      ><i class="bx bx-refresh ml-6" /></button
+    >
+    {#if indiceExercice > 0}
+      <Monter indice={indiceExercice} />
+    {/if}
+    {#if indiceExercice < indiceLastExercice - 1}
+      <Descendre indice={indiceExercice} />
+    {/if}
+    <button
+      type="button"
+      on:click={() => {
+        contenuEtNonCorrection = !contenuEtNonCorrection;
+      }}
+    >
+      {#if contenuEtNonCorrection}
+        <i class="bx bxs-toggle-left ml-6" />
+      {:else}
+        <i class="bx bxs-toggle-right ml-6" />
+      {/if}
+      {contenuEtNonCorrection ? "Correction" : "Consigne"}
+    </button>
+  </h1>
+  {#if visible}
+    {#if contenuEtNonCorrection}
+      <div>{@html contenu}</div>
+    {:else}
+      <div>{@html correction}</div>
+    {/if}
+  {/if}
+</div>
+
+<style>
+</style>
 
 <script lang="ts">
-  import { onMount, tick } from 'svelte'
-  import Monter from './BoutonMonter.svelte'
-  import Descendre from './BoutonDescendre.svelte'
-  import { Mathalea } from '../Mathalea'
-  import type Exercice from "../exercices/ExerciceTs"
+  import { onMount, tick } from "svelte";
+  import Monter from "./BoutonMonter.svelte";
+  import Descendre from "./BoutonDescendre.svelte";
+  import { Mathalea } from "../Mathalea";
+  import type Exercice from "../exercices/ExerciceTs";
 
+  export let directory: string;
+  export let filename: string;
+  export let nbQuestions = undefined;
+  export let indiceExercice: number;
+  export let indiceLastExercice: number;
 
-  export let directory: string
-  export let filename: string
-  export let nbQuestions = undefined
-  export let indiceExercice: number
-  export let indiceLastExercice: number
-
-  let exercice: Exercice
-  let divExercice: HTMLDivElement
-  let visible = true
-  let contenuEtNonCorrection = true
-  let titre: string = ""
-  let contenu: string = ""
-  let correction: string = ""
+  let exercice: Exercice;
+  let divExercice: HTMLDivElement;
+  let visible = true;
+  let contenuEtNonCorrection = true;
+  let titre: string = "";
+  let contenu: string = "";
+  let correction: string = "";
 
   function renderMath() {
-    Mathalea.renderDiv(divExercice)
+    Mathalea.renderDiv(divExercice);
   }
 
   async function newData() {
-    const seed = (Math.random() * 1000).toString()
-    exercice.seed = seed
-    exercice.nouvelleVersion()
+    const seed = (Math.random() * 1000).toString();
+    exercice.seed = seed;
+    exercice.nouvelleVersion();
     // Randomseed
-    contenu = exercice.contenu
-    correction = exercice.contenuCorrection
-    refreshDisplay()
-}
-
-async function refreshDisplay() {
-  if (exercice) {
-    await tick()
-    renderMath()
+    contenu = exercice.contenu;
+    correction = exercice.contenuCorrection;
+    refreshDisplay();
   }
 
-}
+  async function refreshDisplay() {
+    if (exercice) {
+      await tick();
+      renderMath();
+    }
+  }
 
   $: {
     // Dès qu'une de ces variables change, on réactualise le rendu
-    if (visible || contenuEtNonCorrection ||!contenuEtNonCorrection) refreshDisplay()
+    if (visible || contenuEtNonCorrection || !contenuEtNonCorrection)
+      refreshDisplay();
   }
 
   onMount(async () => {
-    exercice = await Mathalea.load(directory, filename)
-    titre = exercice.titre
-    if (nbQuestions) exercice.nbQuestions = nbQuestions
-    exercice.nouvelleVersion()
-    contenu = exercice.contenu
-    correction = exercice.contenuCorrection
-    refreshDisplay()
-   
-  })
+    exercice = await Mathalea.load(directory, filename);
+    titre = exercice.titre;
+    if (nbQuestions) exercice.nbQuestions = nbQuestions;
+    exercice.nouvelleVersion();
+    contenu = exercice.contenu;
+    correction = exercice.contenuCorrection;
+    refreshDisplay();
+  });
 </script>
-
-<div>
-  <button type="button" on:click={() => {visible = !visible}}>{visible ? "Cacher" : "Montrer"}</button>
-  <button type="button" on:click={() => {contenuEtNonCorrection = !contenuEtNonCorrection}}>{contenuEtNonCorrection ? "Voir la correction" : "Voir la consigne"}</button>
-  <button type="button" on:click={newData}>Actualiser</button>
-  {#if indiceExercice > 0}
-    <Monter indice={indiceExercice} />
-  {/if}
-  {#if indiceExercice < indiceLastExercice - 1}
-    <Descendre indice={indiceExercice} />
-  {/if}
-</div>
-{#if visible}
-<div bind:this="{divExercice}">
-  <h1>Exercice {indiceExercice + 1} - {titre}</h1>
-  {#if contenuEtNonCorrection}
-    <div>{@html contenu}</div>
-    {:else}
-    <div>{@html correction}</div>
-  {/if}
-  </div>
-{/if}
-
-<style>
- 
-</style>
