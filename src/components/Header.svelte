66ded6e2de993cb15f9329d136bb06625ef549b5
sylcha <chambon.sylvain@gmail.com>
2022-06-26 16:17:46 +0200

    fix: header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,11 @@
-<script>
-
-</script>
-
-<header class="bg-orange-600">
-<div class="text-white font-medium text-2xl p-3 text-center md:flex justify-between items-center">
-      <div>Coopmaths</div>
-      <div>Calcul mental</div>
-      <div>Professeur</div>
-      <div>À propos</div>
-    </div>
+<header
+  class="bg-orange-600  opacity-100 w-full text-white font-medium text-2xl px-6 py-3 text-center flex justify-between items-center  sticky top-0"
+>
+  <div>Coopmaths</div>
+  <div>Calcul mental</div>
+  <div>Professeur</div>
+  <div>À propos</div>
 </header>
+
+<script>
+</script>
