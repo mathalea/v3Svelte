8bbd64b04b2170b3599be604e98278cd0024d1e7
sylcha <chambon.sylvain@gmail.com>
2022-06-26 16:30:26 +0200

    plus exercice pour le scroll

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,3 +1,24 @@
+<Header />
+<main class="flex-grow px-10">
+  {#each $listeExercices as exercice, i (exercice)}
+    <div animate:flip={{ duration: (d) => 30 * Math.sqrt(d) }}>
+      <Exercice
+        {...exercice}
+        indiceExercice={i}
+        indiceLastExercice={$listeExercices.length}
+      />
+    </div>
+  {/each}
+</main>
+<Footer />
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+</style>
+
 <script lang="ts">
   import { flip } from "svelte/animate";
 
@@ -22,27 +43,28 @@
     nbQuestions: 3,
   };
 
+  const exercice4 = {
+    directory: "5e",
+    filename: "5L10",
+    nbQuestions: 3,
+  };
+  const exercice5 = {
+    directory: "6e",
+    filename: "6C11",
+    nbQuestions: 3,
+  };
+  const exercice6 = {
+    directory: "6e",
+    filename: "6N11",
+    nbQuestions: 3,
+  };
 
-  listeExercices.set([exercice1, exercice2, exercice3]);
+  listeExercices.set([
+    exercice1,
+    exercice2,
+    exercice3,
+    exercice4,
+    exercice5,
+    exercice6,
+  ]);
 </script>
-
-<Header />
-<main class="flex-grow px-10">
-  {#each $listeExercices as exercice, i (exercice)}
-    <div animate:flip={{ duration: (d) => 30 * Math.sqrt(d) }}>
-      <Exercice
-        {...exercice}
-        indiceExercice={i}
-        indiceLastExercice={$listeExercices.length}
-      />
-    </div>
-  {/each}
-</main>
-<Footer />
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  }
-</style>
