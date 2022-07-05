f6fc124b2701d16a50ce0e9bebb2cc4a993c1043
Jean-Claude Lhote <lhotejc@gmail.com>
2022-07-05 07:20:13 +0200

    fix: suppression .js et map des fichiers transpilés

--- src/components/store.js
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store';
-export const listeExercices = writable([]);
-export function moveExercice(liste, iDepart, iArrivee) {
-    liste.splice(iArrivee, 0, liste.splice(iDepart, 1)[0]);
-    return liste;
-}
-//# sourceMappingURL=store.js.map
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './app.css';
-import App from './components/App.svelte';
-const app = new App({
-    target: document.getElementById('app')
-});
-export default app;
-//# sourceMappingURL=main.js.map
