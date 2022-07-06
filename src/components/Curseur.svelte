76b2f301e22519a552e5f4a82e0ed67e008bc3ff
sylcha <chambon.sylvain@gmail.com>
2022-07-06 13:50:50 +0200

    feat: style curseur coopmaths

--- src/components/Curseur.svelte
+++ src/components/Curseur.svelte
@@ -9,5 +9,5 @@
 <div class="flex flex-row justify-start items-center space-x-2">
   <div class="w-3/12">{montant} {montant > 1 ? "nombres " : "nombre "}</div>
   <label class="w-5/12" for={nom}>{titre.toLowerCase()}</label>
-  <input class="form-range w-4/12 h-2 bg-transparent text-coopmaths cursor-pointer" type="range" step="1" {max} min="0" name={nom} id={identifiant} bind:value={montant} />
+  <input class="w-4/12 h-2 bg-transparent text-coopmaths cursor-pointer" type="range" step="1" {max} min="0" name={nom} id={identifiant} bind:value={montant} />
 </div>
