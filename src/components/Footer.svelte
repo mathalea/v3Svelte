93244d502df1a494668362da816f4a0aef62cd0f
Rémi Angot <remi.angot@gmail.com>
2022-07-20 13:54:20 +0200

    fix logo2 + favicon

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <footer class="w-full bg-white h-12 flex flex-row-reverse items-center bottom-0 opacity-100">
-  <img class="px-6" style="height: 30px" src="/src/assets/images/logo2.png" alt="Logo Coopmaths 2" />
+  <img class="px-6" style="height: 30px" src="/assets/images/logo2.png" alt="Logo Coopmaths 2" />
   <a class="pr-6 font-thin" href="https://coopmaths.fr/twitter"> @CoopMaths_fr</a>
   <svg class="h-5 w-5 text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
     <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
